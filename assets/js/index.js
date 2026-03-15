88bf75e8438d79aa8d24fe4dd56e6df16a76c6f8
a.m.mac <alex.mindustry@gmail.com>
2026-03-15 18:39:53 +0800

    add

---
+++ assets/js/index.js
@@ -0,0 +1,41 @@
+const templatetopnav = document.createElement('template');
+
+templatetopnav.innerHTML = `
+  <div class ="topnav">
+    <div class="dropdown">
+        <a class = "topnavbutton">MORE↓</a>
+        <div class="dropdown-content">
+            <a href="/more/Unbl0ck3rs.html">Unbl0ck3rs</a>
+            <a href="/more/Virtual_Machines.html" style="background-color: #FFFF00">Virtual Machines</a>
+            <a href="/more/chat.html" style="background-color: #0000FF">D1sc0rd</a>
+            <a href="/more/G4m3-Requests.html" style="background-color: #00FF00">G4m3 Requests</a>
+        </div>
+    </div>
+    <a href ="/about.html">ABOUT</a>
+    <a href ="/changelog.html">CHANGELOG</a>
+    <a href ="/leaderboard.html">LEADERBOARD</a>
+    <a href ="/g4m3s.html">G4M3S</a>
+    <a href="/index.html">HOME</a>
+</div>
+<div class ="icon">
+    <a href ="/index.html"><img onmouseover="spin(this)" onmouseout="unspin(this)" src="/assets/img/goose.webp" alt="Icon" width="60" height="60"></a>
+</div>
+
+`;
+
+const templategoogleany = document.createElement('template');
+
+templategoogleany.innerHTML = `
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1533872335883359"
+     crossorigin="anonymous"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-HXPPJLK5JN"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-HXPPJLK5JN');
+</script>
+`;
+
+document.body.appendChild(templatetopnav.content);
---
+++ assets/js/main.js
@@ -0,0 +1,170 @@
+var swfobject = {};
+
+swfobject.embedSWF = function(url, cont, width, height){
+    var ruffle = window.RufflePlayer.newest(),
+        player = Object.assign(document.getElementById(cont).appendChild(ruffle.createPlayer()), {
+            width: width,
+            height: height,
+            style: 'width: ' + width + 'px; height: ' + height + 'px',
+        });
+
+    player.load({ url: url });
+}
+
+function iframe_focus() {
+    var toBeFocused = document.getElementById("gameFrame");
+    toBeFocused.focus();
+    console.log("iframe focused");
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    var iframe = document.getElementById("gameFrame");
+    if (iframe){
+    iframe.addEventListener("mouseenter", iframe_focus);
+    }
+});
+
+function openFullscreen() {
+    var elem = document.getElementById("gameFrame");
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+}
+function ruffleFullscreen() {
+    alert("To Fullscreen, Right Click the Game and hit Enter Fullscreen");
+}
+
+function searchbar1() {
+    const searchvalue = document.getElementById('query');
+    var icon_divs = document.getElementById("icon_image");
+    var elem = icon_divs.getElementsByTagName("a")
+    if (searchvalue.value === ''){
+        console.log("Nothing Searched")
+    }
+    else {
+        for (var i = 0; i < elem.length; i++) {
+            if (elem[i].id.toLowerCase().includes(searchvalue.value.toLowerCase())) {
+                elem[i].style.visibility = "visible";
+                elem[i].style.display = "inline-block";
+            } else {
+                elem[i].style.visibility = "collapse";
+                elem[i].style.display = "none";
+            }
+        }
+    }
+
+}
+
+function sorterbuttons(button) {
+    var title = document.getElementById("title");
+    var elem = title.getElementsByTagName("button")
+    for (var i = 0; i < elem.length; i++) {
+        if (elem[i] !== button) {
+            elem[i].style.backgroundColor = "#ffffff";
+            elem[i].style.color = "black";
+        }
+        else{
+            elem[i].style.backgroundColor = "#ff0000";
+            elem[i].style.color = "white";
+        }
+    }
+}
+
+function sorter(category) {
+    var icon = document.getElementById("icon_image");
+    var elem = icon.getElementsByTagName("a")
+    var ads = document.getElementsByClassName("ad")
+    for (var i = 0; i < ads.length; i++) {
+        ads[i].style.visibility = "hidden";
+        ads[i].style.display = "none";
+    }
+
+    for (var i = 0; i < elem.length; i++) {
+        if (elem[i].className !== category) {
+            elem[i].style.visibility = "collapse";
+            elem[i].style.display = "none";
+        }
+        else{
+            elem[i].style.visibility = "visible";
+            elem[i].style.display = "inline-block";
+        }
+    }
+}
+function showall() {
+    var icon = document.getElementById("icon_image");
+    var elem = icon.getElementsByTagName("a")
+    var all = document.getElementById("All")
+
+    sorterbuttons(all)
+    for (var i = 0; i < elem.length; i++) {
+        elem[i].style.visibility = "visible";
+        elem[i].style.display = "inline-block";
+    }
+    var ads = document.getElementsByClassName("ad")
+    for (var i = 0; i < ads.length; i++) {
+        ads[i].style.visibility = "visible";
+        ads[i].style.display = "inline-block";
+    }
+}
+
+
+function enlargeimage(image) {
+    image.style.transform = "scale(1.15)";
+    image.style.transition = "transform 0.25s ease";
+    var fig = image.parentElement.getElementsByTagName("figcaption")[0];
+    if(fig !== undefined){
+        fig.style.visibility = "visible";
+
+    }
+
+
+
+}
+
+
+function notlarge(image) {
+    image.style.transform = "scale(1)";
+    image.style.transition = "transform 0.25s ease";
+    var fig = image.parentElement.getElementsByTagName("figcaption")[0];
+    if(fig !== undefined){
+        fig.style.visibility = "hidden";
+
+    }
+
+
+}
+
+function showimage(element) {
+    var child = element.parentElement.getElementsByTagName("a")[0];
+    child.style.visibility = "visible";
+}
+function unimage(element) {
+    var child = element.parentElement.getElementsByTagName("a")[0];
+    child.style.visibility = "hidden";
+}
+function showimagea(element) {
+    element.style.visibility = "visible";
+}
+function unimagea(element) {
+    element.style.visibility = "hidden";
+}
+
+
+function spin(image){
+    image.style.transform = "rotate(359deg)";
+    image.style.transition = "transform 0.25s ease";
+
+
+}
+
+function unspin(image){
+    image.style.transform = "rotate(0deg)";
+}
+
+
+
